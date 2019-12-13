2bea0697137c0fae368967c2be95aea07e28851e
AironDev <airondev@gmail.com>
2019-12-13 02:44:33 +0100

    Merge branch 'version_2'

--- note_app/data.js
+++ note_app/data.js
@@ -2,11 +2,11 @@ const Data = [
 	{
 		id: 1,
 		title: 'Cook Egusi Soup',
-		note: 'Its been long I ate egusi soup, so I feeel its a high time I prepare a delicous meal, hopefully tommoro.',
+		content: 'Its been long I ate egusi soup, so I feeel its a high time I prepare a delicous meal, hopefully tommoro.',
 		url: '#',
 		created: "2/11/2019",
 		key: "",
-		tag:"",
+		tag:"Book",
 		book: [{
 			title: "",
 			author: "",
@@ -19,33 +19,78 @@ const Data = [
 			time: "",
 			role: "",
 			status: "",
+			type: "",
 		}],
 		todo: [{
 			status: "",
 			priority:"",
 		}],
-		note: [{
-
+		bookmark: [{
+			url: ""
 		}]
 	},
 
 	{
 		id: 2,
 		title: 'Code on Python',
-		note: 'Python is the language of the future, If you want to stay relevant in the next comming years, go learn python NOW!',
+		content: 'Python is the language of the future, If you want to stay relevant in the next comming years, go learn python NOW!',
 		url: '#',
 		created: "3/10/2019",
 		image: '/assets/images/layout.png',
-		key: ""
+		key: "",
+		tag:"Todo",
+		book: [{
+			title: "",
+			author: "",
+			chapter: "",
+			page: "",
+		}],
+		event: [{
+			venue: "",
+			date: "",
+			time: "",
+			role: "",
+			status: "",
+			type: "",
+		}],
+		todo: [{
+			status: "",
+			priority:"",
+		}],
+		bookmark: [{
+			url: ""
+		}]
 	},
 
 		{
 		id: 3,
 		title: 'Visit Uncle Pius',
-		note: 'I have to pay uncle Pius a visit, before this month runs, its been long since the last time we met',
+		content: 'I have to pay uncle Pius a visit, before this month runs, its been long since the last time we met',
 		url: '#',
 		created: "9/26/2019",
 		image: '/assets/images/layout.png',
-		key: ""
+		key: "",
+		tag:"Event",
+		book: [{
+			title: "",
+			author: "",
+			chapter: "",
+			page: "",
+		}],
+		event: [{
+			venue: "",
+			date: "",
+			time: "",
+			role: "",
+			status: "",
+			type: "",
+		}],
+		todo: [{
+			status: "",
+			priority:"",
+		}],
+		bookmark: [{
+			url: ""
+		}]
 	},
 ]
