48f48c845ae00ea424c9aa2f0d47153eafda6f6f
AironDev <airondev@gmail.com>
2021-01-22 08:11:47 +0100

    vue project structured

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hello from About</h1>
+    <NavBarMain />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBarMain from '@/components/NavBarMain.vue'
+
+export default {
+  name: 'About',
+  components: {
+    NavBarMain
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hello from Home</h1>
+    <NavBarMain />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBarMain from '@/components/NavBarMain.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    NavBarMain
+  }
+}
+</script>
